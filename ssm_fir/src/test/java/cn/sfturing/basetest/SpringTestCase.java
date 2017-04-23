722a64b431695254dd121fe6d08dd171f28c9d60
sfturing <motionnnnn@sxh>
2017-04-23 14:37:46 +0800

    springMVC

--- ssm_fir/src/test/java/cn/sfturing/basetest/SpringTestCase.java
+++ ssm_fir/src/test/java/cn/sfturing/basetest/SpringTestCase.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 //指定bean注入的配置文件  
-@ContextConfiguration(locations = { "classpath:applicationContext.xml" })  
+@ContextConfiguration(locations = { "classpath:spring/applicationContext.xml" })  
 //使用标准的JUnit @RunWith注释来告诉JUnit使用Spring TestRunner  
 @RunWith(SpringJUnit4ClassRunner.class)  
 public abstract class SpringTestCase extends AbstractJUnit4SpringContextTests{  
